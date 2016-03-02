61e9502a785f2cb1f66fe03ad725533e7ea2dbfa
Matthew O'Connor <matthewocsecond@gmail.com>
2016-03-02 17:22:15 +0000

    travis CI inclusion

--- srv/travis-dev.js
+++ srv/travis-dev.js
@@ -1,6 +1,21 @@
-var STATS = process.env.STATS || 'localhost'
+'use strict'
+
 require('seneca')()
   .use('../travis.js')
-  .client({ host:'localhost', pin:{role:'travis', cmd:'*'}})
-  .listen(44005)
-  .repl(43005)
+  .add('role:info,req:part',function (args,done) {
+    done()
+
+    this.act('role:travis,cmd:get', {name:args.name},function (err, mod) {
+      if (err) return done(err);
+
+        this.act('role:info,res:part,part:travis', {name:args.name,data:mod.data$()})
+      })
+  })
+
+  .add('role:travis,info:change', function (args,done) {
+    done()
+    this.act('role:travis,cmd:get', {name:args.name,update:true})
+  })
+  .use('../node_modules/seneca-mesh', {auto:true, pins:['role:travis','role:info,req:part'], model:'publish'} )
+  
+
