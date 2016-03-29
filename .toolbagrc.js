a37ccc3ed87cf0d6e637a676362ccee9d8714379
Shane Lacey <shane.lacey@nearform.com>
2016-03-29 11:56:30 +0100

    Added toolbag plugins to package

--- .toolbagrc.js
+++ .toolbagrc.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const StatsCollector = require('toolbag/lib/plugins/toolbag_plugin_stats_collector')
-const UdpReporter = require('toolbag/lib/plugins/toolbag_plugin_udp_reporter')
+const StatsCollector = require('toolbag-plugin-stats-collector')
+const UdpReporter = require('toolbag-plugin-udp-reporter')
 
 module.exports = function (defaults, callback) {
   callback(null, {
