80466aa289804a5e0709f91690cf1b2e39961843
Gege Pincin <gege@pincin.com>
2016-04-08 01:12:28 -0400

    changing opts

--- srv/start.js
+++ srv/start.js
@@ -1,7 +1,14 @@
 'use strict'
 
+var opts = {
+  redis: {
+    host: 'localhost',
+    port: process.env.redis_PORT
+  }
+}
+
 require('seneca')()
-.use('redis-store', { host:'localhost', port:6379 })
+.use('redis-store', opts.redis)
 .use('entity')
 .use('../travis.js')
 .add('role:info,req:part',function (args,done) {
